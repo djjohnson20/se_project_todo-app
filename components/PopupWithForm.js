08cd6c49a307737538cbc6c1a3c55c6e5ec170c5
Donald Johnson <djjohnson380@gmail.com>
2024-12-26 15:33:45 -0700

    Deleted duplicate code

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -22,7 +22,6 @@ class PopupWithForm extends Popup {
     this._popupForm.addEventListener("submit", (evt) => {
       evt.preventDefault();
       const inputValues = this._getInputValues();
-      this._getInputValues();
       this._handleFormSubmit(inputValues);
       this._popupForm.reset();
     });
